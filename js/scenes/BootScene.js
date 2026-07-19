daf4a876716dcbaad00c9aa69815f4d3d2dc8b40
watcharin2045-wq <watcharin.2045@gmail.com>
2026-07-19 22:21:06 +0700

    Update BootScene.js

--- js/scenes/BootScene.js
+++ js/scenes/BootScene.js
@@ -1,1 +1,22 @@
 
+export default class BootScene extends Phaser.Scene{
+
+    constructor(){
+
+        super("BootScene");
+
+    }
+
+    preload(){
+
+        console.log("Loading...");
+
+    }
+
+    create(){
+
+        this.scene.start("MenuScene");
+
+    }
+
+}
