c7b317d4a575deb5a09e3ef727dfb9263d1beaf6
watcharin2045-wq <watcharin.2045@gmail.com>
2026-07-19 22:24:19 +0700

    Update GameScene.js

--- js/scenes/GameScene.js
+++ js/scenes/GameScene.js
@@ -1,1 +1,64 @@
 
+export default class GameScene extends Phaser.Scene{
+
+    constructor(){
+
+        super("GameScene");
+
+    }
+
+    create(){
+
+        this.add.text(
+
+            40,
+
+            30,
+
+            "⭐ 0",
+
+            {
+
+                fontSize:"32px",
+
+                color:"#000"
+
+            }
+
+        );
+
+        this.add.rectangle(
+
+            640,
+
+            350,
+
+            720,
+
+            520,
+
+            0xffffff
+
+        );
+
+        this.add.text(
+
+            470,
+
+            650,
+
+            "Version 3",
+
+            {
+
+                fontSize:"32px",
+
+                color:"#1565c0"
+
+            }
+
+        );
+
+    }
+
+}
