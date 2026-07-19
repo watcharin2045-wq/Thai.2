ed6783eb360bf062df72535dbd59c540166a8bf3
watcharin2045-wq <watcharin.2045@gmail.com>
2026-07-19 22:21:31 +0700

    Update MenuScene.js

--- js/scenes/MenuScene.js
+++ js/scenes/MenuScene.js
@@ -1,1 +1,64 @@
 
+export default class MenuScene extends Phaser.Scene{
+
+    constructor(){
+
+        super("MenuScene");
+
+    }
+
+    create(){
+
+        this.add.text(
+
+            370,
+
+            120,
+
+            "🎮 ตู้คีบคำศัพท์",
+
+            {
+
+                fontSize:"52px",
+
+                color:"#000",
+
+                fontStyle:"bold"
+
+            }
+
+        );
+
+        const btn=this.add.text(
+
+            470,
+
+            350,
+
+            "▶ เริ่มเกม",
+
+            {
+
+                fontSize:"44px",
+
+                backgroundColor:"#FFD54F",
+
+                padding:20,
+
+                color:"#000"
+
+            }
+
+        );
+
+        btn.setInteractive();
+
+        btn.on("pointerdown",()=>{
+
+            this.scene.start("GameScene");
+
+        });
+
+    }
+
+}
