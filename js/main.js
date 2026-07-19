34990db29b4fd163d2a4b87fcf65b6009a6ce097
watcharin2045-wq <watcharin.2045@gmail.com>
2026-07-19 19:00:13 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,55 +1,38 @@
-const config={
-
-type:Phaser.AUTO,
-
-width:1280,
-
-height:720,
-
-parent:"game",
-
-backgroundColor:"#9EE8FF",
-
-scene:{
-
-preload:preload,
-
-create:create,
-
-update:update
-
-}
-
+const config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    parent: "game",
+    backgroundColor: "#9EDFFF",
+    scene: {
+        preload,
+        create,
+        update
+    }
 };
 
-const game=new Phaser.Game(config);
+new Phaser.Game(config);
 
-function preload(){
+function preload() {
+}
+
+function create() {
+
+    this.add.text(430, 30, "🎮 ตู้คีบคำศัพท์", {
+        fontSize: "42px",
+        color: "#000",
+        fontStyle: "bold"
+    });
+
+    this.add.rectangle(640, 330, 700, 420, 0xffffff)
+        .setStrokeStyle(8, 0xdddddd);
+
+    this.add.text(560, 300, "Version 3", {
+        fontSize: "36px",
+        color: "#1565c0"
+    });
 
 }
 
-function create(){
-
-this.add.text(
-
-450,
-
-40,
-
-"🎮 ตู้คีบคำศัพท์",
-
-{
-
-fontSize:"40px",
-
-color:"#000"
-
-}
-
-);
-
-}
-
-function update(){
-
+function update() {
 }
