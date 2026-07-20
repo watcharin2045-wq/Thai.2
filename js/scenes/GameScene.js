e8fcef5d915a2fc7945d32c7fa8bf2233a65ee91
watcharin2045-wq <watcharin.2045@gmail.com>
2026-07-20 21:10:51 +0700

    Update GameScene.js

--- js/scenes/GameScene.js
+++ js/scenes/GameScene.js
@@ -1,62 +1,179 @@
+export default class GameScene extends Phaser.Scene {
 
-export default class GameScene extends Phaser.Scene{
-
-    constructor(){
-
+    constructor() {
         super("GameScene");
-
     }
 
-    create(){
+    create() {
+
+        //==========================
+        // Background
+        //==========================
+
+        this.cameras.main.setBackgroundColor("#9EDFFF");
+
+        //==========================
+        // Score
+        //==========================
 
         this.add.text(
-
-            40,
-
-            30,
-
+            1080,
+            20,
             "⭐ 0",
-
             {
-
-                fontSize:"32px",
-
-                color:"#000"
-
+                fontSize: "30px",
+                color: "#000"
             }
-
         );
+
+        //==========================
+        // Cabinet Shadow
+        //==========================
 
         this.add.rectangle(
+            645,
+            365,
+            730,
+            530,
+            0x999999,
+            0.25
+        );
 
+        //==========================
+        // Cabinet
+        //==========================
+
+        this.add.rectangle(
             640,
-
-            350,
-
+            360,
             720,
-
             520,
-
             0xffffff
+        );
 
+        //==========================
+        // Glass
+        //==========================
+
+        this.add.rectangle(
+            640,
+            340,
+            680,
+            450,
+            0xdff6ff
+        );
+
+        //==========================
+        // Bottom Panel
+        //==========================
+
+        this.add.rectangle(
+            640,
+            575,
+            720,
+            90,
+            0x666666
+        );
+
+        //==========================
+        // LED
+        //==========================
+
+        for(let i=0;i<18;i++){
+
+            this.add.circle(
+                300+i*38,
+                110,
+                6,
+                0xffee00
+            );
+
+        }
+
+        for(let i=0;i<18;i++){
+
+            this.add.circle(
+                300+i*38,
+                610,
+                6,
+                0xff66aa
+            );
+
+        }
+
+        for(let i=0;i<12;i++){
+
+            this.add.circle(
+                300,
+                150+i*38,
+                6,
+                0x55ff55
+            );
+
+        }
+
+        for(let i=0;i<12;i++){
+
+            this.add.circle(
+                980,
+                150+i*38,
+                6,
+                0x55ccff
+            );
+
+        }
+
+        //==========================
+        // Rope
+        //==========================
+
+        this.add.rectangle(
+            640,
+            150,
+            4,
+            120,
+            0x444444
+        );
+
+        //==========================
+        // Claw
+        //==========================
+
+        this.add.circle(
+            640,
+            210,
+            18,
+            0xdddddd
+        );
+
+        this.add.line(
+            0,
+            0,
+            640,
+            210,
+            625,
+            240,
+            0x444444
+        );
+
+        this.add.line(
+            0,
+            0,
+            640,
+            210,
+            655,
+            240,
+            0x444444
         );
 
         this.add.text(
-
-            470,
-
+            500,
             650,
-
-            "Version 3",
-
+            "Version 3.2",
             {
-
-                fontSize:"32px",
-
-                color:"#1565c0"
-
+                fontSize:"28px",
+                color:"#000"
             }
-
         );
 
     }
