b4c3bd0f8cb7eda641ebc44b9a99959c3355f252
watcharin2045-wq <watcharin.2045@gmail.com>
2026-07-20 21:25:56 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-import BootScene from "./scenes/BootScene.js";
-import MenuScene from "./scenes/MenuScene.js";
-import GameScene from "./scenes/GameScene.js";
-
 const config={
 
     type:Phaser.AUTO,
@@ -41,20 +37,3 @@ const config={
     ]
 
 }
-    parent: "game",
-
-    backgroundColor: "#9EDFFF",
-
-    scene: [
-
-        BootScene,
-
-        MenuScene,
-
-        GameScene
-
-    ]
-
-};
-
-new Phaser.Game(config);
