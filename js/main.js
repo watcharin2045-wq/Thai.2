cdfcfa183829e1dcbffb75531ad4864951ccde03
watcharin2045-wq <watcharin.2045@gmail.com>
2026-07-21 08:58:09 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,67 @@
+// ========================================
+// Thai Claw Game
+// V4.0
+// Main Entry
+// ========================================
+
+
+// ----------------------------------------
+// นำเข้า Scene
+// ----------------------------------------
+
 import BootScene from "./scenes/BootScene.js";
-import MenuScene from "./scenes/MenuScene.js";
-import GameScene from "./scenes/GameScene.js";
+
+import PreloadScene from "./scenes/PreloadScene.js";
+
+
+// ----------------------------------------
+// ตั้งค่า Phaser
+// ----------------------------------------
 
 const config = {
+
+    // เลือก Renderer อัตโนมัติ
     type: Phaser.AUTO,
 
+
+    // ขนาดเกม
     width: 1280,
+
     height: 720,
 
+
+    // HTML Element ที่ใช้แสดงเกม
     parent: "game",
 
+
+    // ปรับขนาดตามหน้าจอ
+    scale: {
+
+        mode: Phaser.Scale.FIT,
+
+        autoCenter: Phaser.Scale.CENTER_BOTH
+
+    },
+
+
+    // สีพื้นหลังเริ่มต้น
     backgroundColor: "#9EDFFF",
 
+
+    // ระบบ Scene
     scene: [
+
         BootScene,
-        MenuScene,
-        GameScene
+
+        PreloadScene
+
     ]
+
 };
+
+
+// ----------------------------------------
+// สร้างเกม Phaser
+// ----------------------------------------
 
 new Phaser.Game(config);
